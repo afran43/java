019d076cc0d14db3c759a2b64343034a75f7f93e
afran43 <afranali43@gmail.com>
2022-08-15 11:22:01 +0600

    jave

---
+++ oporation.js
@@ -0,0 +1,25 @@
+var onioPrice = 20;
+var eggprice = 70;
+// console.log(onioPrice)
+// console.log(eggprice)
+// console.log(onioPrice + eggprice);
+
+/* < Jog kora ++++++++ */
+var totalPrice = onioPrice + eggprice;
+// console.log(totalPrice)
+
+// biok kora -------->
+var priceDifference = onioPrice - eggprice;
+// console.log(priceDifference)
+
+// gon 2*2= 4
+var orangePrice = 20;
+var quantiy = 7;
+var totalCost = orangePrice * quantiy;
+// console.log(totalCost)
+
+// Division (bag 10/5=2)
+var money = 500;
+var Player = 10;
+var eachPlayer = money / Player;
+console.log(eachPlayer)
---
+++ shothand.js
@@ -0,0 +1,42 @@
+// // variable declaration
+// var tomotoPrice = 25;
+
+// // variable value update
+// tomotoPrice = 30;
+
+// // add somthing to the previous value
+// var price1 = 31;
+// var price2 = 10;
+
+// // set a new value
+// // price1 =35;
+
+// // add some value to the existing value
+// price1 = price1 + 10;
+// // // console.log(price1)
+
+// // addition to the same variable shorthand
+// // += means adding some value to existing value
+// price1 += 10;
+// // console.log(price1)
+
+// price1 = price1 - 10;
+// // console.log(price1)
+
+// var age = 15;
+// age = age + 1;
+// // ++means adding 1 to the existing Value
+// age++;
+// // console.log(age)
+
+// var age = 15;
+// age = age - 1;
+// // -- means remove 1 to the existing Value
+// age--;
+// // console.log(age)
+
+
+var firstName = 'Afran';
+var lastName = 'Ali';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
